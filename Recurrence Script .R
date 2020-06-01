c3f0c73ad19d61f24e29c1eaf8a969911c522d3a
KatieFish <kjf214@lehigh.edu>
2020-06-01 13:18:58 -0500

    changing recurrence script to be more efficient

--- Recurrence Script .R
+++ Recurrence Script .R
@@ -1,35 +1,48 @@
-#KJF 3/20/18 
+#KJF 3/31/20
 #script calculates the probability of any given gene or intergenic region being mutated 
 #n times, controlling for gene size. W303_Position_File.txt is required
 #Input data should be in the form of a data frame with 2 columns. Ex. 
-#Feature  #n 
-#ACE2     5
-#CTS1     12
-#...      ... 
+#Dataset  #Chromosome #Position
 
 
 #upon annotating intergenic mutations, ORF position file can be replaced with position file
 
-Calculate_Recurrence_Probability <- function (Observed_Gene_Counts, W303_Position_File){
-#first add an empty column to observed counts dataframe to hold lambda
-Observed_Gene_Counts$Lambda<-NA 
-#calculate genome size 
-genome_size<-sum(na.omit(W303_Position_File[, 4]))
-Nmutations<- sum(Observed_Gene_Counts$n)
-for (i in 1:nrow(Observed_Gene_Counts)){ #iterate through rows of observed data
-  for (j in 1:nrow(W303_Position_File)){ #iterate through rows of annotation file 
-    if (Observed_Gene_Counts[i, 1] == W303_Position_File[j, 6]){ #find correct gene
-       gene_size<- W303_Position_File[j, 4]
-       Observed_Gene_Counts[i, 3]<- (gene_size/genome_size)*Nmutations
-        break
-    }
+Calculate_Recurrence_Probability <- function (Observed_Data, W303_Position_File){
+  refdf<- W303_Position_File
+  refdf$FeatureID<-c(1:nrow(refdf))
+  Observed_Data->df
+  df$Feature<-NA
+  df$FeatureID<-NA
+  df$Size<-NA
+
+  for (j in 1:nrow(df)){
+    refrow<-which(refdf$Chromosome == df$Chromosome[j] &
+                                   refdf$Begin<= df$Position[j] &
+                                   refdf$End >= df$Position[j])
+    df$Feature[j]<-as.character(refdf$Feature[refrow])
+    df$FeatureID[j]<-refdf$FeatureID[refrow]
+    df$Size[j]<-refdf$Size[refrow]
   }
-}
-Observed_Gene_Counts$"P{n}"<- ((((Observed_Gene_Counts$Lambda)^(Observed_Gene_Counts$n))/
-  (factorial(Observed_Gene_Counts$n)))*exp(-(Observed_Gene_Counts$Lambda)))
-Observed_Gene_Counts$"logP{n}"<- log10(Observed_Gene_Counts$`P{n}`)
-Result.df<- Observed_Gene_Counts[order(Observed_Gene_Counts$"P{n}"), ]
-Result.df$BF.adjusted<- p.adjust(Result.df$"P{n}", method="bonferroni")
-Result.df$BH.adjusted<- p.adjust(Result.df$"P{n}", method= "BH")
+
+#calculate genome size 
+genome_size<-sum(na.omit(refdf$Size))
+Nmutations<- nrow(df)
+
+recurrence<-data.frame(table(df$FeatureID))
+colnames(recurrence)<-c("FeatureID", "n")
+df<-merge(df, recurrence, by="FeatureID")
+z<-which(colnames(df)=="Position")
+df<-df[-z]
+df<-unique(df)
+
+# add an empty column to observed counts dataframe to hold lambda
+df$Lambda<-(df$Size/genome_size)*Nmutations 
+
+df$"Psubn"<- ((((df$Lambda)^(df$n))/
+  (factorial(df$n)))*exp(-(df$Lambda)))
+#df$"logP{n}"<- log10(df$`P{n}`)
+Result.df<- df[order(df$Psubn), ]
+#Result.df$BF.adjusted<- p.adjust(Result.df$"P{n}", method="bonferroni")
+Result.df$BH.adjusted<- p.adjust(Result.df$Psubn, method= "BH")
 return(Result.df)
 }
